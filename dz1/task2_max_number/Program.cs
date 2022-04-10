a7dad057f8f296e70bcff6c8a6ea0fb2e4481f0b
dvvinokurov <dmitvitvin@gmail.com>
2022-04-10 14:07:11 +0300

    add check equal

--- dz1/task2_max_number/Program.cs
+++ dz1/task2_max_number/Program.cs
@@ -1,6 +1,5 @@
 ﻿int numberA = 5;
 int numberB = 38;
-if(numberA > numberB)
-Console.WriteLine($"{numberA} больше чем {numberB}");
-else 
-Console.WriteLine($"{numberB} больше чем {numberA}");
+if(numberA > numberB) Console.WriteLine($"{numberA} больше чем {numberB}");
+if(numberB > numberA) Console.WriteLine($"{numberB} больше чем {numberA}");
+if(numberA == numberB) Console.WriteLine($"Оба числа равны {numberA} ");
