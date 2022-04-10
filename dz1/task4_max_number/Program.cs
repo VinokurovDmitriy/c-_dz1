80120cfe42461ff9be9b57961323c564d5cd025a
dvvinokurov <dmitvitvin@gmail.com>
2022-04-10 13:33:40 +0300

    add task6

---
+++ dz1/task4_max_number/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = 34;
+int numberB = 27;
+int numberC = 38;
+int max = numberA;
+if(numberB > max)
+max = numberB;
+if(numberC > max)
+max = numberC;
+Console.WriteLine($"Мааксимоальное число = {max}");
