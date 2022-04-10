0c0fb531b71bdbb6c7d5b5b5d434ed5d4ba33704
dvvinokurov <dmitvitvin@gmail.com>
2022-04-10 14:48:15 +0300

    add task description

--- dz1/task2_max_number/Program.cs
+++ dz1/task2_max_number/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 5;
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int numberA = 5;
 int numberB = 38;
 if(numberA > numberB) Console.WriteLine($"{numberA} больше чем {numberB}");
 if(numberB > numberA) Console.WriteLine($"{numberB} больше чем {numberA}");
