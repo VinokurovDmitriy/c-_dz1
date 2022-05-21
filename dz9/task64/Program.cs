56e0cc21fb54204a9e2896f04fddce8d9f7bb0de
dvvinokurov <dmitvitvin@gmail.com>
2022-05-21 19:11:19 +0300

    add task64

---
+++ dz9/task64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int NumSum( int num )
+{
+    if(num < 10) 
+    {
+        return num;
+    }
+    else 
+    {
+       return ((num % 10) + NumSum( num / 10 ));
+    }
+}
+
+Console.WriteLine($"Сумма цифр числа {n} = {NumSum(n)}");
